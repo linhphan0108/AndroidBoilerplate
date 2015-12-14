a5a4f48bbfa32168f0e43e018e8ef19f8298e1ee
linh phan <linhphan0108@gmail.com>
2015-12-14 08:54:28 +0700

    add getting screenshot feature

--- app/src/main/java/com/linhphan/androidboilerplate/util/ViewUtil.java
+++ app/src/main/java/com/linhphan/androidboilerplate/util/ViewUtil.java
@@ -3,6 +3,7 @@ package com.linhphan.androidboilerplate.util;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -39,5 +40,12 @@ public class ViewUtil {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
         view.requestFocus();
         inputMethodManager.showSoftInput(view, 0);
+    }
+
+    public Bitmap getScreenShot(View view){
+        view.buildDrawingCache();
+        Bitmap screenShot = view.getDrawingCache();
+        view.destroyDrawingCache();
+        return screenShot;
     }
 }
